42cb66798dbe0087b82287ffdfdfb3920b4836d5
shiivamgupta <shivam16121993@gmail.com>
2022-12-15 15:17:36 +0530

    Python Library for MAX30101 & MAX30102

---
+++ Python Library/hrdump.py
@@ -0,0 +1,17 @@
+# max30101 max30102 Library
+# -*-coding:utf-8-*-
+
+import max30102
+
+m = max30102.MAX30102()
+
+red, ir = m.read_sequential(1000)
+
+with open("./red.log", "w") as f:
+    for r in red:
+        f.write("{0}\n".format(r))
+with open("./ir.log", "w") as f:
+    for r in ir:
+        f.write("{0}\n".format(r))
+
+m.shutdown()
